867218c6707e7e9d54a50f43b2bd2d2b25b795fc
sapargal13v <sapargal13v@mail.ru>
2021-04-15 13:27:11 +0600

    initial commit

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Hjaseyyh Grursht on 07.04.2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
